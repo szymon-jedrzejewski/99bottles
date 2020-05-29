7f054f6251b0ab19d758621b36269ad8c2740912
Szymon Jedrzejewski <szymon219@poczta.fm>
2020-05-29 18:43:21 +0200

    Finish writing tests

---
+++ src/main/java/Song.java
@@ -0,0 +1,30 @@
+public class Song {
+    public String getVerse(int verse) {
+        return getAmountOfBottles(verse) + " of beer on the wall. " + getAmountOfBottles(verse) + " of beer.\n" +
+                getActivity(verse) + ". " + getAmountOfBottles(verse - 1) + " of beer on the wall.";
+    }
+
+    private String getActivity(int verse) {
+        return verse == 0 ? "Go to the store and buy some more" : "Take one down and pass it around";
+    }
+
+    private String getAmountOfBottles(int verse) {
+        if (verse == -1) return "99 bottles";
+        if (verse == 0) return "No more bottles";
+        if (verse == 1) return verse + " bottle";
+
+        return verse + " bottles";
+    }
+
+    public String getVerses(int start, int end) {
+        String verses = "";
+        for (int i = start; i >= end; i--) {
+            verses += getVerse(i) + "\n\n";
+        }
+        return verses.substring(0, verses.length() - 2);
+    }
+
+    public String getWholeSong() {
+        return getVerses(99, 0);
+    }
+}
